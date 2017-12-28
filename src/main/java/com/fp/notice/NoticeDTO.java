494b72672395fa9f55babb5d9e8d602ea24f22fc
qkrwogk110 <qkrwogk110@naver.com>
2017-12-28 13:32:00 +0900

    Revert "Basic_form"
    This reverts commit e13f42a93fd96db39c00e53a021c711e4e04f26c.

--- src/main/java/com/fp/notice/NoticeDTO.java
+++
@@ -1,7 +0,0 @@
-package com.fp.notice;
-
-import com.fp.board.BoardDTO;
-
-public class NoticeDTO extends BoardDTO{
-
-}
--- src/main/java/com/fp/review/ReviewDTO.java
+++
@@ -1,7 +0,0 @@
-package com.fp.review;
-
-import com.fp.board.BoardDTO;
-
-public class ReviewDTO extends BoardDTO{
-
-}
